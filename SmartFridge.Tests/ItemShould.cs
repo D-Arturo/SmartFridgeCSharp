7e49a8e8bb4efc6e1d0bf1797b588425d62cfcd3
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2024-09-22 15:29:34 +0200

    [test] - calculate days until item expiration

---
+++ SmartFridge.Tests/ItemShould.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using JetBrains.Annotations;
+using SmartFridge;
+using Xunit;
+
+namespace SmartFridge.Tests;
+
+[TestSubject(typeof(Item))]
+public class ItemShould
+{
+
+    [Fact]
+    public void CalculateDaysUntilExpiration()
+    {
+        var sweetPotato = new Item("Sweet Potato", DateTime.Today.AddDays(2));
+        Assert.Equal(2, sweetPotato.CalculateDaysUntilExpiration());
+    }
+}
